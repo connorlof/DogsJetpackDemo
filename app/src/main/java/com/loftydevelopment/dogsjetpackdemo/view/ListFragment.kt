a354c801f8774523827a4eb5e7c41b48f903af66
Connor Loftus <connorlof@gmail.com>
2020-05-03 20:09:37 -0400

    test commit

--- app/src/main/java/com/loftydevelopment/dogsjetpackdemo/view/ListFragment.kt
+++ app/src/main/java/com/loftydevelopment/dogsjetpackdemo/view/ListFragment.kt
@@ -16,7 +16,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment..
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
